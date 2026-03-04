9d79c6b2190389e4b71b74ee629131a526ce8f5f
thathanathip679 <thanathip.nan@gmail.com>
2026-03-04 23:25:18 +0700

    lab23_1.cpp

--- lab23_1.cpp
+++ lab23_1.cpp
@@ -20,20 +20,67 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
+void importDataFromFile(string filename, vector<string> &n, vector<int> &s, vector<char> &g){
+    ifstream F(filename);
+    string T;
+    while(getline(F,T)){
+        const char *text = T.c_str();
+        char format[] = "%[^:]: %d %d %d";
+        char Name[100];
+        int x,y,z,sum;
+        sscanf(text,format,Name,&x,&y,&z);
+        string N = Name;
+        n.push_back(N);
+        sum = x+y+z;
+        s.push_back(sum);
+        g.push_back(score2grade(sum));
+    }
 
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command:" << endl;
+    string input;
+    getline(cin,input);
+    int A = input.find(" ");
+    command = input.substr(0,A);
+    key = input.substr(A+1);
 }
 
-void searchName(){
-
+void searchName(vector<string> &names, vector<int> &scores, vector<char> &grades, string key){
+    
+    for(unsigned i = 0; i < names.size() ; i++){
+        if(key == toUpperStr(names[i])){
+            cout << "---------------------------------" << endl;
+            cout << names[i] << "'s score = " << scores[i] << endl;
+            cout << names[i] << "'s grade = " << grades[i] << endl;
+            cout << "---------------------------------" << endl;
+            break;
+        }
+        
+        if(i == names.size()-1){
+        cout << "---------------------------------" << endl;
+        cout << "Cannot found." << endl;
+        cout << "---------------------------------" << endl;
+        }
+    }
+    
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> &names, vector<int> &scores, vector<char> &grades, string key){
+    bool z;
+    cout << "---------------------------------" << endl;
+    for(unsigned i = 0 ; i < grades.size() ; i++){
+        if(key[0] == grades[i]){
+            z = true;
+            cout << names[i] << " (" << scores[i] << ")" << endl;
+        }
+        
+        if(i == grades.size()-1 && !z){
+        cout << "Cannot found." << endl;
+        }
+    }
+    cout << "---------------------------------" << endl;
 }
 
 
